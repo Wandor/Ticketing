cda56aba82d877d5479db50fde45118682e32acf
Wandor <jwandor97@gmail.com>
2019-09-30 16:31:17 +0300

    Functionality on TicketController

--- Ticketing/Controllers/Api/TicketController.cs
+++ Ticketing/Controllers/Api/TicketController.cs
@@ -26,6 +26,51 @@ namespace Ticketing.Controllers.Api
         {
             return Ok(new { data = _context.Tickets.Where(x => x.TicketID.Equals(TicketID)).ToList() });
         }
+        // GET: api/Ticket/Customer
+        [HttpGet("Customer/{customerId}")]
+        public IActionResult GetTicketCustomer([FromRoute]Guid CustomerID)
+        {
+            return Ok(new { data = _context.Tickets.Where(x => x.CustomerID.Equals(CustomerID)).ToList() });
+        }
 
+        // POST: api/Ticket
+        [HttpPost]
+        public async Task<IActionResult> PostTicket([FromBody] Ticket ticket)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                if (ticket.TicketID == Guid.Empty)
+                {
+                    //Contact contact = _context.Contact.Where(x => x.contactId.Equals(ticket.contactId)).FirstOrDefault();
+                    ticket.TicketID = Guid.NewGuid();
+                    ticket.CustomerID = contact.customerId;
+                    _context.Tickets.Add(ticket);
+
+                    await _context.SaveChangesAsync();
+
+                    return Ok(new { success = true, message = "Add new data success." });
+                }
+                else
+                {
+                    _context.Update(ticket);
+
+                    await _context.SaveChangesAsync();
+
+                    return Ok(new { success = true, message = "Edit data success." });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Ok(new { success = false, message = ex.Message });
+            }
+
+
+        }
     }
 }
